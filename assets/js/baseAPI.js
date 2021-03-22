48bc2885df9a0ac97eb85851b16f18aba2763a98
yjm <592199007@qq.com>
2021-03-22 16:47:34 +0800

    完成了index

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -6,4 +6,13 @@ $.ajaxPrefilter(function(options) {
             Authorization: localStorage.getItem('token') || ''
         };
     }
+    options.complete = function(res){
+        // console.log(res);
+        // console.log(res.responseJSON);
+        if(res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！")
+        {
+            localStorage.removeItem("token");
+            location.href="/login.html";
+        }
+    }
 });
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,17 @@
 $(function() {
+    //获取用户信息
     getUserInfo();
+
+    // 为退出按钮绑定事件
+    $("#btnLogout").on("click",function(){
+        // console.log("ok");
+        layer.confirm('确定退出登录？', {icon: 3, title:'提示'}, function(index){
+            //do something
+            location.href = "/login.html";
+            localStorage.removeItem("token");
+            layer.close(index);
+          });
+    })
 })
 
 function getUserInfo() {
@@ -9,8 +21,9 @@ function getUserInfo() {
         success: function(res) {
             if(res.status !== 0){
                 layui.layer.msg("获取用户信息失败！");
+                return;
             }
-
+            // console.log(res);
             // 调用renderAvatar渲染用户头像
             renderAvatar(res.data);
         }
@@ -18,6 +31,19 @@ function getUserInfo() {
 }
 
 function renderAvatar(user) {
+    // 设置欢迎用户文本
     let name = user.nickname || user.username;
-    
+    $("#welcome").html("欢迎&nbsp;&nbsp;"+name);
+
+    // 渲染头像
+    if(user.user_pic !== null){
+        // 渲染图片头像
+        $('.layui-nav-img').attr('src',user.user_pic).show();
+        $('.text-avatar').hide();
+    }else{
+        // 渲染文本头像
+        $('.layui-nav-img').hide();
+        var first = name[0].toUpperCase();
+        $('.text-avatar').html(first).show();
+    }
 }
